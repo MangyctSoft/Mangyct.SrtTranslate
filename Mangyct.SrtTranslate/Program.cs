175e3fb7a1c8849bc15d18e550ef9704fb35f0fb
Mangyct <andrei-df@mail.ru>
2020-03-26 15:11:39 +0300

    Добавьте файлы проекта.

---
+++ Mangyct.SrtTranslate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mangyct.SrtTranslate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
